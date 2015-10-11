83184933418cd4cd7de53cdd026d34714cd8dd7c
Jeremy Ortiz <jrortiz369@hotmail.com>
2015-10-11 16:08:34 -0500

    Finished The BluePurple Expansion
    Added Blue and Purple cards.
    Cards are sent out properly.
    Can get both cards at once, each labeled which card they are upon exit.
    Can add your own cards to the code in order to get a better more
    interesting deck.
    Enjoy!

--- BluePurpleExpansion.py
+++ BluePurpleExpansion.py
@@ -4,38 +4,49 @@ import tkinter as tk
 from tkinter import *
 
 
+#global variables because I'm having trouble with the deletion/lines and calling both decks at the same time.
+purpleCards = ["ZOMBIES!!!","A Bomb's ticking down!", "Save the falling babies!", "A Damsel in Distress",
+                    "A Bachelor in Distress", "Save Obama!", "Your hero is naked"]
+blueCards = ["Ocean", "China", "Disney Land", "Earth's Moon", "Atillan"]
+
+welcomeMessage = '''Welcome to the Blue Deck and Purple Deck program for SuperFight.
+This deck allows you to use the Blue Deck and Purple Deck along with your
+SuperFight game!
+Blue Deck button will give you a location.
+Purple Deck button will give you a situation.'''
 
 class Application(tk.Frame):
+
 
     def infoCallBack(self):
         #This will print information about the Program.
         tk.messagebox.showinfo("INFO", '''Superfight is a hilarious card game based on fights between characters with superpowers and super problems.
 Check it out at: http://www.superfightgame.com/
 This is an independent project that I have created to save me money on the Blue and Purple Deck.
-Written by Jeremy Ortiz''')
+Written by Jeremy Ortiz \n''' + "="*86 + welcomeMessage)
 
         
     def callBlueDeck(self):
         #This will take a card from the blue deck
         self.text.configure(state="normal")
-        self.text.insert(END,"="*80)
-        self.text.insert(END,"BlueTest\n")
+        self.text.delete('1.0', END)
+        self.text.insert(END,"Blue card: " + blueCards[random.randrange(0,len(blueCards),1)] + "\n")
         self.text.configure(state="disabled")
 
     def callPurpleDeck(self):
         #This will take a card from the purple deck
+    
         self.text.configure(state="normal")
-        self.text.insert(END,"="*80)
-        self.text.insert(END,"PurpleTest\n")
+        self.text.delete('1.0', END)
+        self.text.insert(END,"PurpleCard: " + purpleCards[random.randrange(0,len(purpleCards),1)] + "\n")
         self.text.configure(state="disabled")
 
     def callBothDecks(self):
         self.text.configure(state="normal")
-        self.text.insert(END,"="*80)
-        self.text.insert(END,"BlueTest\n")
-        self.text.insert(END,"PurpleTest\n")
+        self.text.delete('1.0', END)
+        self.text.insert(END,"Blue card: " + blueCards[random.randrange(0,len(blueCards),1)] + "\n")
+        self.text.insert(END,"PurpleCard: " + purpleCards[random.randrange(0,len(purpleCards),1)] + "\n")
         self.text.configure(state="disabled")
-
 
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
@@ -46,12 +57,7 @@ Written by Jeremy Ortiz''')
             #Textbox that will print out the cards taken from the deck
             self.text = tk.Text(self)
             self.text.pack()
-            self.text.insert(END,'''Welcome to the Blue Deck and Purple Deck program for SuperFight.
-This deck allows you to use the Blue Deck and Purple Deck along with your
-SuperFight game!
-Blue Deck button will give you a location.
-Purple Deck button will give you a situation.
-''')
+            self.text.insert(END,welcomeMessage)
             self.text.configure(state="disabled")
             self.text.insert(END,"test")
 
@@ -59,18 +65,21 @@ Purple Deck button will give you a situation.
             self.blueDeck = tk.Button(self, fg="blue")
             self.blueDeck["text"] = "Blue Deck"
             self.blueDeck["command"] = self.callBlueDeck
+            self.text.insert(END, "="*80)
             self.blueDeck.pack(side="top")
 
             #Purple Deck button widget
             self.purpleDeck = tk.Button(self, fg="purple")
             self.purpleDeck["text"] = "Purple Deck"
             self.purpleDeck["command"] = self.callPurpleDeck
+            self.text.insert(END, "="*80)
             self.purpleDeck.pack()
 
             #Purple Deck button widget
             self.purpleDeck = tk.Button(self)
             self.purpleDeck["text"] = "Purple and Blue Deck"
             self.purpleDeck["command"] = self.callBothDecks
+            self.text.insert(END, "="*80)
             self.purpleDeck.pack()
             
             #Info Button Widget
